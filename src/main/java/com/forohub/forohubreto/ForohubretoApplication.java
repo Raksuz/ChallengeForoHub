474a40797a2e60d9cff85b152e95cbbbf54742c5
Carlos Lopez <cxrloz.1112@gmail.com>
2024-07-15 19:58:18 -0600

    ProyectoForoHub

---
+++ src/main/java/com/forohub/forohubreto/ForohubretoApplication.java
@@ -0,0 +1,13 @@
+package com.forohub.forohubreto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForohubretoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForohubretoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forohub/forohubreto/ForohubretoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forohub.forohubreto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForohubretoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
